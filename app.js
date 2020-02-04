fe2353eee3d715e6a7863afe4ac2a6a046cbb91e
happy-2-happy <thinkgood2all99@gmail.com>
2020-02-04 16:47:40 +0530

    Fixed Eslint problems

--- app.js
+++ app.js
@@ -1,36 +1,41 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+
+const app = express();
 const port = 3002;
-var morgan = require('morgan');
-var bodyParser = require('body-parser');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 require('dotenv').config();
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
- 
-// parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(morgan((tokens, req, res) => {
+// parse application/json
+app.use(bodyParser.json());
+
+app.use(
+  morgan((tokens, req, res) => {
     return [
       tokens.method(req, res),
       tokens.url(req, res),
       tokens.status(req, res),
-      tokens.res(req, res, 'content-length'), '-',
-      tokens['response-time'](req, res), 'ms'
-    ].join(' ')
-  }));
+      tokens.res(req, res, 'content-length'),
+      '-',
+      tokens['response-time'](req, res),
+      'ms'
+    ].join(' ');
+  })
+);
 
-var util = require('./app/api/v1/util/router');
+const util = require('./app/api/v1/util/router');
 
 app.use('/v1/util', util);
 
-app.listen(port, (err) => {
-    if (err) {
-        console.error(`Error while connecting ${port}!`)
-    } else {
-        console.log(`Server started in port ${port}`)
-    }
-})
+app.listen(port, err => {
+  if (err) {
+    console.error(`Error while connecting ${port}!`);
+  } else {
+    console.log(`Server started in port ${port}`);
+  }
+});
 
 module.exports = app;
